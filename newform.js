c964fbf0fe7da671c0bcfd90dbbcf5fcf314f8e5
Satyasai <satyasai1997@gmail.com>
2023-11-26 09:43:18 +0530

    get-commit

--- newform.js
+++ newform.js
@@ -15,7 +15,7 @@ function saveData(e){
         "phone": phone
     
     };
-    axios.post('https://crudcrud.com/api/8a622d3a626f4e46b78176a59b45a066/appointmentData', my_obj)
+    axios.post('https://crudcrud.com/api/6eef386a0af74e5baa92849d56744691/appointmentData', my_obj)
     .then(res => console.log(res.data))
     .catch(err => console.log(err))
     
@@ -59,9 +59,59 @@ function saveData(e){
         localStorage.removeItem(email);
     };
 
-
-
     document.getElementById('username').value = null;
     document.getElementById('useremail').value = null;
     document.getElementById('userphone').value = null;
  }
+
+ window.addEventListener('DOMContentLoaded',()=>{
+    axios.get('https://crudcrud.com/api/6eef386a0af74e5baa92849d56744691/appointmentData')
+    .then(res => {console.log(res.data)
+    for(let i=0; i< res.data.length; i++){
+        const userData = Object.values(res.data[i])
+        const li = document.createElement('li')
+        li.innerText = userData[1]+'-'+ userData[2] +'-'+ userData[3];
+        var ul = document.getElementById('user-list');
+        ul.appendChild(li);
+
+
+        let name = document.querySelector('#username').value;
+        let email = document.querySelector('#useremail').value;
+        let phone = document.querySelector('#userphone').value;
+    
+        const deleteBtn = document.createElement('input')
+        const editBtn = document.createElement('input')
+    
+        deleteBtn.type = 'button';
+        deleteBtn.value = 'delete'
+    
+        editBtn.type = 'button';
+        editBtn.value = 'edit';
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+    
+        deleteBtn.addEventListener('click', removeData);
+        function removeData(e){
+            e.preventDefault();
+            deleteBtn.parentElement.innerText = ' ';
+            li.style.listStyle='none';
+            localStorage.removeItem(email);
+        };
+    
+        editBtn.addEventListener('click', editData);
+        function editData(e){
+            e.preventDefault();
+            document.getElementById('username').value = name;
+            document.getElementById('useremail').value = email;
+            document.getElementById('userphone').value = phone;
+            editBtn.parentElement.innerText = ' ';
+            li.style.listStyle='none';
+            localStorage.removeItem(email);
+        };
+
+    }
+    }
+    )
+    .catch(err => console.log(err))
+    
+    })
